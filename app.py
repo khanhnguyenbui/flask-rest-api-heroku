2afc7b3e4e12eade74c3495c626bbd3929955d57
khanhnguyenbui <khanhsbn@gmail.com>
2019-01-24 12:48:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -56,8 +56,83 @@ class All_Movies(Resource):
 #        return {'Movies': list(map(lambda x: x.json(), Movies.query.all()))}
         return "test"
     
+users = [
+    {
+        "name": "Nicholas",
+        "age": 42,
+        "occupation": "Network Engineer"
+    },
+    {
+        "name": "Elvin",
+        "age": 32,
+        "occupation": "Doctor"
+    },
+    {
+        "name": "Jass",
+        "age": 22,
+        "occupation": "Web Developer"
+    }
+]
+
+class User(Resource):
+    def get(self, name):
+        for user in users:
+            if(name == user["name"]):
+                return user, 200
+        return "User not found", 404
+
+    def post(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument("age")
+        parser.add_argument("occupation")
+        args = parser.parse_args()
+
+        for user in users:
+            if(name == user["name"]):
+                return "User with name {} already exists".format(name), 400
+
+        user = {
+            "name": name,
+            "age": args["age"],
+            "occupation": args["occupation"]
+        }
+        users.append(user)
+        return user, 201
+
+    def put(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument("age")
+        parser.add_argument("occupation")
+        args = parser.parse_args()
+
+        for user in users:
+            if(name == user["name"]):
+                user["age"] = args["age"]
+                user["occupation"] = args["occupation"]
+                return user, 200
+        
+        user = {
+            "name": name,
+            "age": args["age"],
+            "occupation": args["occupation"]
+        }
+        users.append(user)
+        return user, 201
+
+    def delete(self, name):
+        global users
+        users = [user for user in users if user["name"] != name]
+        return "{} is deleted.".format(name), 200
+
+class UserAll(Resource):
+    def get(self):
+        return users
+
+    
 api.add_resource(All_Movies, '/')
 api.add_resource(Movies_List, '/<string:movie>')
+api.add_resource(User, "/user/<string:name>")
+api.add_resource(UserAll, "/user")
 
 if __name__=='__main__':
     
